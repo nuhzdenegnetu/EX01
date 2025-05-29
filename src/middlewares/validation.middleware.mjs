9d82f38dc10d24382329ca9b1482319e2ac97925
nuhzdenegnetu <venternevertony@gmail.com>
2025-05-29 18:28:51 +0300

    feat: add EJS support for articles and enhance error handling

--- src/middlewares/validation.middleware.mjs
+++
@@ -1,7 +0,0 @@
-export const validateUserData = (req, res, next) => {
-    const { name, email } = req.body;
-    if (!name || !email) {
-        return res.status(400).send('Invalid user data');
-    }
-    next();
-};
